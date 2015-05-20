f85132527a625afb1036f2a25a2b0b43fdb8c2a2
Pros259 <spencer.prost@pnnl.gov>
2015-05-20 08:59:10 -0700

    Updated test
    Updated test cases.

--- AmpsBoxTests/Data/SignalTableTests.cs
+++ AmpsBoxTests/Data/SignalTableTests.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO.Ports;
+    using System.Linq;
     using System.Text;
 
     using AmpsBoxSdk.Commands;
@@ -37,23 +38,28 @@
             table.UpdateExecutionData(new SignalTableExecutionData("test", "", 1, 0.0, TimeUnits.Microseconds));
             table.Children.Add(
                 new SignalTable(
-                    new Waveform(new ChannelAddress("1"), 0).AddSignalElement(
+                    new Waveform(new ChannelAddress("2"), 0).AddSignalElement(
                         new AnalogStepElement(new ChannelAddress("2"), new Voltage(5), 1000))).UpdateExecutionData(new SignalTableExecutionData("test1", "desc", 2, 500, TimeUnits.Microseconds)));
-          
+
+            table.Children.First().Children.Add(
+               new SignalTable(
+                   new Waveform(new ChannelAddress("3"), 0).AddSignalElement(
+                       new AnalogStepElement(new ChannelAddress("3"), new Voltage(5), 1000))).UpdateExecutionData(new SignalTableExecutionData("test3", "desc1", 3, 600, TimeUnits.Microseconds)));
+
 
             var formater = new AmpsBoxSignalTableCommandFormatter();
             var data = formater.FormatTable(table, new AmpsClockConverter(16000000));
             Console.WriteLine(data);
 
-            var falkorSerialPort = new FalkorSerialPort(new SerialPort("COM18") {BaudRate = 19200, Handshake = Handshake.XOnXOff, DataBits = 8, Parity = Parity.Even, Encoding = Encoding.ASCII, StopBits = StopBits.One});
-            AmpsBox box = new AmpsBox(new AmpsBoxCOMReader(falkorSerialPort), new AmpsCOMCommandFormatter() );
+            var falkorSerialPort = new FalkorSerialPort(new SerialPort("COM18") { BaudRate = 19200, Handshake = Handshake.XOnXOff, DataBits = 8, Parity = Parity.Even, Encoding = Encoding.ASCII, StopBits = StopBits.One });
+            AmpsBox box = new AmpsBox(new AmpsBoxCOMReader(falkorSerialPort), new AmpsCOMCommandFormatter());
             box.Communicator.Open();
-           var version =  box.GetVersionAsync();
+            var version = box.GetVersionAsync();
             version.Wait();
             var v = version.Result;
 
             var response = box.LoadTimeTableAsync(table);
-            
+
             response.Wait();
             var r = response.Result;
             var error = box.GetError().Result;
