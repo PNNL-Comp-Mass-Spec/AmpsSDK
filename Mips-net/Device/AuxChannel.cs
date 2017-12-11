d790f75ccdc5c1d382c8c2b826a82053d06d1d90
Spencer Prost <spencer.prost@pnnl.gov>
2017-12-11 11:12:35 -0800

    Added new enum type to represent the new aux / offset channel values.

---
+++ Mips-net/Device/AuxChannel.cs
@@ -0,0 +1,15 @@
+﻿namespace Mips.Device
+{
+    public enum AuxChannel
+    {
+        Unknown = 0,
+        Aux1 = 101,
+        Aux2 = 102,
+        Aux3 = 103,
+        Aux4 = 104,
+        Offset1A = 105,
+        Offset1B = 106,
+        Offset1C = 107,
+        Offset1D = 108
+    }
+}
