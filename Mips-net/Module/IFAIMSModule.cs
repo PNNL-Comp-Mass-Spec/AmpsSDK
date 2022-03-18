b3122d5819d5bd998cc5f03245b2ea05787d2eb7
Cameron Giberson <cameron.giberson@pnnl.gov>
2022-03-18 12:52:11 -0700

    add new FAIMS commands

--- Mips-net/Module/IFAIMSModule.cs
+++ Mips-net/Module/IFAIMSModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Reactive;
+﻿using Mips.Device;
+using System.Reactive;
 using System.Threading.Tasks;
 
 namespace Mips.Module
@@ -7,5 +8,42 @@ namespace Mips.Module
 	{
 		Task<Unit> SetPositiveOutput(int slope,int offset);
 		Task<Unit> SetNegativeOutput(int slope, int offset);
+		Task<Unit> SetEnableWaveformGeneration(State state);
+		Task<State> GetEnableWaveformGeneration();
+		Task<Unit> SetDriveLevelPercent(double level);
+		Task<double> GetDriveLeverPercent();
+		Task<double> GetWaveformGenerationPower();
+		Task<double> GetPositivePeakOutputVoltageKW();
+		Task<double> GetNegativePeakOutputVoltageKW();
+		Task<Unit> SetEnableOutputVoltageLocking(State state);
+		Task<State> GetOutputVoltageLockStatus();
+		Task<Unit> SetOutputVoltageSetPointKV(double voltage);
+		Task<Unit> SetEnableSystemAutotune();
+		Task<string> GetSystemAutotuneStatus();
+		Task<Unit> SetCVOutputDcVoltageSetpoint(double voltage);
+		Task<double> GetCVOutputDcVoltageSetpoint();
+		Task<double> GetCvOutputVoltageReadback();
+		Task<Unit> SetBiasOutputDcVoltageSetpoint(double voltage);
+		Task<double> GetBiasOutputDcVoltageSetpoint();
+		Task<double> GetBiasOutputDcVoltagReadback();
+		Task<Unit> SetOffsetOutputVoltageSetpoint(double voltage);
+		Task<double> GetOffsetOutputVoltageSetpoint();
+		Task<double> GetOffsetOutputVoltageReadback();
+		Task<Unit> SetCVScanStartVoltage(double voltage);
+		Task<double> GetCVScanStartVoltage();
+		Task<Unit> SetCVScanEndVoltage(double voltage);
+		Task<double> GetCVScanEndVoltage();
+		Task<Unit> SetScanDuration(double seconds);
+		Task<double> GetScanDuration();
+		Task<Unit> SetScanLoopCount(int count);
+		Task<int> GetScanLoopCount();
+		Task<Unit> StartLinearScan(Status status);
+		Task<Status> GetLinearScanStatus();
+		Task<Unit> SetStepScanDuration(int milliseconds);
+		Task<int> GetStepScanDuration();
+		Task<Unit> SetStepScanStepCount(int count);
+		Task<int> GetStepScanStepCount();
+		Task<Unit> StartStepScan(Status status);
+		Task<Status> GetStepScanModeStatus();
 	}
 }
